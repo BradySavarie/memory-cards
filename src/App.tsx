fd2a58f6c0b63a8117c7e3e44034136ca385e028
Brady <brady.savarie@hotmail.com>
2023-06-24 19:06:00 -0400

    Fetch and convert api data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
-function App() {
-    return <div>Test</div>;
+import { useEffect, useState } from 'react';
+
+interface Movie {
+    id: number;
+    title: string;
+    posterURL: string;
+    imdbId: string;
 }
 
-export default App;
+export default function App() {
+    const [rawMovieData, setRawMovieData] = useState<Movie[]>([]);
+    const [moviesArray, setMoviesArray] = useState<Movie[]>([]);
+
+    const getRawMovieData = async () => {
+        const resp = await fetch('https://api.sampleapis.com/movies/classic');
+        const json = await resp.json();
+        setRawMovieData(json);
+    };
+
+    const generateMoviesArray = () => {
+        const numbers: number[] = [];
+        const movies: Movie[] = [];
+
+        while (numbers.length < 25) {
+            const number = Math.floor(Math.random() * 84) + 1;
+
+            if (!numbers.includes(number)) {
+                numbers.push(number);
+            }
+        }
+
+        numbers.forEach((number) => {
+            const targetMovie = rawMovieData.find(
+                (movie) => movie.id === number
+            );
+            if (targetMovie !== undefined) {
+                movies.push(targetMovie);
+            }
+        });
+
+        setMoviesArray(movies);
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await getRawMovieData();
+        };
+        fetchData();
+        generateMoviesArray();
+    }, []);
+
+    return <div>{JSON.stringify(moviesArray)}</div>;
+}
